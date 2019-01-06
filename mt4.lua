40790f23cd69a184f6a7a4dbb7df63b0df0252fb
mumin guler <mumin16@hotmail.com>
2019-01-06 13:44:46 +0300

    Update mt4.lua

--- mt4.lua
+++ mt4.lua
@@ -18,3 +18,20 @@ end
 for k, v in pairs(VOLUME) do
    print(k, v)
 end
+
+
+
+--for mt5 (ansi)
+
+OPEN={} HIGH={} LOW={} CLOSE={} VOLUME={}
+local i=1
+for line in io.lines("USDTRYH1.csv") do
+    local date, open, high, low, close, volume,io_ = 
+    line:match("%s*(.-),%s*(.-),%s*(.-),%s*(.-),%s*(.-),%s*(.+),")
+    OPEN[i]=open HIGH[i]=high LOW[i]=low CLOSE[i]=close VOLUME[i]=volume
+    i=i+1
+end
+
+for k, v in pairs(VOLUME) do
+   print(k, v)
+end
